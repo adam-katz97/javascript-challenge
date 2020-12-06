107a1bdc15fd4557bbdcafc264e2530afed9f023
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-12-06 10:36:33 -0600

    makes a forEach loop that displays all the data in a table format on the webpage.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var table = d3.select('table table-striped');
+var tbody = d3.select('tbody');
+data.forEach(function(ufo) {
+    var row = tbody.append('tr')
+    
+    Object.entries(ufo).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    })
+
+})
+
