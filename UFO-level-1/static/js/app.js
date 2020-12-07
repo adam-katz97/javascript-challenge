2dfc32ab6f31aa88c136fed1474dfb6a525a5614
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-12-07 10:39:24 -0600

    makes working data filter.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,7 +14,7 @@ data.forEach(function(ufo) {
     });
 
 });
-var button = d3.select('#button');
+var button = d3.select('#filter-btn');
 var form = d3.select('#form');
 button.on("click", runEnter);
 form.on("submit", runEnter);
@@ -23,6 +23,18 @@ function runEnter() {
     d3.event.preventDefault();
     var inputElement = d3.select('#datetime');
     var inputValue = inputElement.property('value');
-    var filterDate = tableData.filter((sighting) == sighting.datetime === inputValue);
-    console.log(filterDate)
+    var filterDate = tableData.filter((sighting) => sighting.datetime === inputValue);
+    tbody.html("")
+    filterDate.forEach(function(ufo) {
+        var row = tbody.append('tr');
+        
+        Object.entries(ufo).forEach(function([key, value]){
+            
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    
+    });
+    
+    
 }  
